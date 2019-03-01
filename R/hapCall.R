4811698405877fa2a32c395b1155a6e11f056847
Qiang Hu <qiang.hu@roswellpark.org>
2019-03-01 11:25:30 -0500

    add last update data to man

--- R/hapCall.R
+++ R/hapCall.R
@@ -2,7 +2,9 @@
 #'
 #' The workflow runs HaplotypeCaller from GATK4 in GVCF mode on a
 #' single sample according to the GATK Best Practices (June 2016),
-#' scattered across intervals.
+#' scattered across intervals. The workflow from github,
+#' gatk4-germline-snps-indels (last update: 7/23/2018) was cloned to
+#' this package.
 #' 
 #' @format A `cwlStepParam` object.
 #' \describe{
--- R/mc3.R
+++ R/mc3.R
@@ -1,6 +1,8 @@
 #' TCGA MC3 pipeline
 #'
-#' The MC3 somatic variant calling pipeline.
+#' The MC3 somatic variant calling pipeline. The mc3 workflow (last
+#' update: 11/5/2018) from github was cloned to the package.
+#' 
 #' @format A `cwlStepParam` object.
 #' \describe{
 #'  \item{call_variants}{To call somatic variants by multiple pipelines}
