a9e1be471cda12a9711c0f8732a88a78680a0d4f
Qiang Hu <qiang.hu@roswellpark.org>
2019-03-01 15:10:26 -0500

    fix format for submission

--- R/RcwlPipelines-package.R
+++ R/RcwlPipelines-package.R
@@ -1,7 +1,16 @@
 #' RcwlPipelines
 #'
-#' A package for a collection of Rcwl pipelines.
-#'
+#' A package for a collection of Rcwl pipelines. Currently four
+#' pipeplines have been collected in the package.
+#' 
+#' \describe{
+#' \item{\link{rnaseq_Sf}, \link{multiqc}:}{RNASeq alignment,
+#' QC and quantification pipeline}
+#' \item{\link{alignMerge}:}{DNASeq alignment}
+#' \item{\link{GAlign}, \link{hapCall}, \link{jdCall}:}{GATK4
+#' pipeline for Germline calling}
+#' \item{\link{mc3}:}{TCGA MC3 pipeline for Somatic calling}
+#' }
 #' @import Rcwl
 #' @docType package
 #' @name RcwlPipelines
