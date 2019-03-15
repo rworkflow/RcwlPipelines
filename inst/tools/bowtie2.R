5c61f6c82a28c9cda95c4814e7d20373261f6e7a
marcogao2019 <48455426+marcogao2019@users.noreply.github.com>
2019-03-15 16:30:44 -0400

    Update bowtie2.R

--- inst/tools/bowtie2.R
+++ inst/tools/bowtie2.R
@@ -1,13 +1,24 @@
-## bowtie2_2.3.0-2-deb
+## bowtie2:v2.2.9_cv2
+## Be careful: bowtie2-build genome.fa genome.fa 
+## Basename of .bt2 files must be exactly equal to "genome.fa" which is the reference_in
 
 p1 <- InputParam(id = "threadN", type = "int", prefix = "-p", position = 1)
-# p2 <- InputParam(id = "IndexPrefix", type = "string", prefix = "-x", position = 2) #To be solved 
+p2 <- InputParam(id = "IndexPrefix", type = "File", prefix = "-x",
+                 valueFrom = "$(self.dirname + '/' + self.nameroot)",
+                 secondaryFiles = c("$(self.nameroot + '.1.bt2')",
+                                    "$(self.nameroot + '.2.bt2')", 
+                                    "$(self.nameroot + '.3.bt2')",
+                                    "$(self.nameroot + '.4.bt2')",
+                                    "$(self.nameroot + '.rev.1.bt2')", 
+                                    "$(sefl.nameroot + '.rev.2.bt2')"), 
+                 position = 2) 
 p3 <- InputParam(id = "fastq", type = "File[]", prefix = "-U", position = 3)
 o1 <- OutputParam(id = "sam", type = "File", glob = "*.sam")
-req1 <- list(class = "DockerRequirement", 
-             dockerPull = "biocontainers/bowtie2:2.3.0-2-deb")
+req1 <- list(class = "InlineJavascriptRequirement")
+req2 <- list(class = "DockerRequirement", 
+             dockerPull = "biocontainers/bowtie2:v2.2.9_cv2")
 bowtie2 <- cwlParam(baseCommand = "bowtie2",
-                    requirements = list(req1),
+                    requirements = list(req1, req2),
                     arguments = list("-S", "output.sam"),
                     inputs = InputParamList(p1, p2, p3),
                     outputs = OutputParamList(o1))
