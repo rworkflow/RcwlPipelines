6df44d355ddb1e30661a065094ebec8816233a44
marcogao2019 <48455426+marcogao2019@users.noreply.github.com>
2019-03-15 12:42:26 -0400

    Update bowtie2.R

--- inst/tools/bowtie2.R
+++ inst/tools/bowtie2.R
@@ -4,7 +4,7 @@ p1 <- InputParam(id = "threads", type = "int", prefix = "-p", position = 1)
 p2 <- InputParam(id = "IndexPrefix", type = "string", prefix = "-x", position = 2)
 p3 <- InputParam(id = "FQ1", type = "File", prefix = "-U", position = 3)
 # p4 <- InputParam(id = "FQ2", type = "File?", prefix = "-2", position = 4)
-o1 <- OutputParam(id = "sam", type = "File", prefix = "-S", glob = "*.sam")
+o1 <- OutputParam(id = "sam", type = "File", glob = "*.sam")
 req1 <- list(class = "DockerRequirement", 
              dockerPull = "biocontainers/bowtie2:2.3.0-2-deb")
 bowtie2 <- cwlParam(baseCommand = "bowtie2",
