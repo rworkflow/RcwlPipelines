ae51e3fdb9f94160d7e70adf2d516b09be5e93a0
marcogao2019 <48455426+marcogao2019@users.noreply.github.com>
2019-03-15 13:12:12 -0400

    Update bowtie2.R

--- inst/tools/bowtie2.R
+++ inst/tools/bowtie2.R
@@ -1,14 +1,13 @@
 ## bowtie2_2.3.0-2-deb
 
-p1 <- InputParam(id = "threads", type = "int", prefix = "-p", position = 1)
+p1 <- InputParam(id = "threadN", type = "int", prefix = "-p", position = 1)
 p2 <- InputParam(id = "IndexPrefix", type = "string", prefix = "-x", position = 2)
-p3 <- InputParam(id = "FQ1", type = "File", prefix = "-U", position = 3)
-# p4 <- InputParam(id = "FQ2", type = "File?", prefix = "-2", position = 4)
+p3 <- InputParam(id = "fastq", type = "File[]", prefix = "-U", position = 3)
 o1 <- OutputParam(id = "sam", type = "File", glob = "*.sam")
 req1 <- list(class = "DockerRequirement", 
              dockerPull = "biocontainers/bowtie2:2.3.0-2-deb")
 bowtie2 <- cwlParam(baseCommand = "bowtie2",
-                    requirements = list(req1), 
+                    requirements = list(req1),
+                    arguments = list("-S", "output.sam"),
                     inputs = InputParamList(p1, p2, p3),
-                    output = OutputParamList(o1),
-                    stdout = "bowtie2Output.sam")
+                    outputs = OutputParamList(o1))
