80474075761b0f9138ef4cb9cf104ef2334db56f
marcogao2019 <48455426+marcogao2019@users.noreply.github.com>
2019-03-15 12:31:44 -0400

    Update bowtie2.R

--- inst/tools/bowtie2.R
+++ inst/tools/bowtie2.R
@@ -1,7 +1,7 @@
 ## bowtie2_2.3.0-2-deb
 
 p1 <- InputParam(id = "threads", type = "int", prefix = "-p", position = 1)
-p2 <- InputParam(id = "Ref Index prefix", type = "string", prefix = "-x", position = 2)
+p2 <- InputParam(id = "IndexPrefix", type = "string", prefix = "-x", position = 2)
 p3 <- InputParam(id = "FQ1", type = "File", prefix = "-U", position = 3)
 # p4 <- InputParam(id = "FQ2", type = "File?", prefix = "-2", position = 4)
 o1 <- OutputParam(id = "sam", type = "File", prefix = "-S", glob = "*.sam")
