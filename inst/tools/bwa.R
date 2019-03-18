e3cebf9b31bd0a0737651aa5b57c05b8540c8266
Qiang Hu <qiang.hu@roswellpark.org>
2019-03-18 16:41:40 -0400

    change String to File for bwa ref

--- inst/tools/bwa.R
+++ inst/tools/bwa.R
@@ -1,7 +1,8 @@
 ## bwa mem
 p1 <- InputParam(id = "threads", type = "int", prefix = "-t", position = 1)
 p2 <- InputParam(id = "RG", type = "string", prefix = "-R", position = 2)
-p3 <- InputParam(id = "Ref", type = "string", position = 3)
+p3 <- InputParam(id = "Ref", type = "File", position = 3,
+                 secondaryFiles = c(".amb", ".ann", ".bwt", ".pac", ".sa"))
 p4 <- InputParam(id = "FQ1", type = "File", position = 4)
 p5 <- InputParam(id = "FQ2", type = "File?", position = 5)
 o1 <- OutputParam(id = "sam", type = "File", glob = "*.sam")
