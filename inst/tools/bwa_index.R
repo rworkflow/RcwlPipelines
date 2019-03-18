afe1a6275627dc1d55b67189d8849c4845fb3a4b
marcogao2019 <48455426+marcogao2019@users.noreply.github.com>
2019-03-18 16:19:31 -0400

    Update bwa_index.R

--- inst/tools/bwa_index.R
+++ inst/tools/bwa_index.R
@@ -1,17 +1,22 @@
-### bwa_index
+### bwa_index : only for long genome (-a bwtsw)
 
-p1 <- InputParam(id = "Ref", type = "File")
-o1 <- OutputParam(id = "idx", type = "File",
-                  valueFrom = "$(self.dirname + '/' + self.basename)",
-                  secondaryFiles = c("$(self.basename + '.amb')",
-                                    "$(self.basename + '.ann')", 
-                                    "$(self.basename + '.bwt')",
-                                    "$(self.basename + '.pac')",
-                                    "$(self.basename + '.sa')") 
 req1 <- list(class = "DockerRequirement",
              dockerPull = "biocontainers/bwa:v0.7.15_cv3")
-bwa <- cwlParam(baseCommand = c("bwa", "index"),
-                requirements = list(req1),
-                arguments = list("-a", "bwtsw")
-                inputs = InputParamList(p1),
-                output = OutputParamList(o1))
+req2 <- list(class = "InitialWorkDirRequirement", 
+             listing = list("$(inputs.Ref)"))
+req3 <- list(class = "InlineJavascriptRequirement")
+
+p1 <- InputParam(id = "Ref", type = "File", valueFrom = "$(self.basename)")
+o1 <- OutputParam(id = "idx", type = "File", 
+                  glob = "$(inputs.Ref.basename)",
+                  secondaryFiles = c("$(inputs.Ref.basename + '.amb')",
+                                     "$(inputs.Ref.basename + '.ann')", 
+                                     "$(inputs.Ref.basename + '.bwt')",
+                                     "$(inputs.Ref.basename + '.pac')",
+                                     "$(inputs.Ref.basename + '.sa')") )
+
+bwa_index <- cwlParam(baseCommand = c("bwa", "index"), 
+                      requirements = list(req1, req2, req3),
+                      arguments = list("-a", "bwtsw"), 
+                      inputs = InputParamList(p1), 
+                      outputs = OutputParamList(o1))
