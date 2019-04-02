d60a381facb0c0db92569a295a8da4b96ee12ba6
Qiang Hu <qiang.hu@roswellpark.org>
2019-04-02 14:46:04 -0400

    fix testthat

--- tests/testthat/test_all.R
+++ tests/testthat/test_all.R
@@ -1,11 +1,10 @@
 
 test_that("total pipelines number", {
     expect_equal(nrow(data(package="RcwlPipelines")$results),
-                 7)})
+                 7)
+})
 
 test_that("check source tool scripts", {
     tools <- cwlTools(tempdir())
-    fpath <- bfcinfo(tools)$fpath
-    expect_equal(ncol(sapply(fpath, source)),
-    length(fpath))
+    expect_is(tools, "BiocFileCache")
 })
