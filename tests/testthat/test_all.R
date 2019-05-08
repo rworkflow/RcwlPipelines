1ab67c907c4394b8b148d4b3c77239018abf4053
Qiang Hu <qiang.hu@roswellpark.org>
2019-05-08 14:59:15 -0400

    fix test

--- tests/testthat/test_all.R
+++ tests/testthat/test_all.R
@@ -1,7 +1,8 @@
 
+## mc3 removed
 test_that("total pipelines number", {
     expect_equal(nrow(data(package="RcwlPipelines")$results),
-                 7)
+                 6)
 })
 
 test_that("check source tool scripts", {
