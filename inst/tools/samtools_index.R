77668f1fecf555b43648c49b1e459629bb69dd92
Qiang Hu <qiang.hu@roswellpark.org>
2019-05-22 11:54:02 -0400

    fix samtools_index bug for cwltool 2019

--- inst/tools/samtools_index.R
+++ inst/tools/samtools_index.R
@@ -1,6 +1,7 @@
 ## Index bam
 p1 <- InputParam(id = "bam", type = "File", position = 1)
-o1 <- OutputParam(id = "idx", type = "File", glob = "$(inputs.bam.basename)", secondaryFiles = ".bai")
+o1 <- OutputParam(id = "idx", type = "File", glob = "$(inputs.bam.basename)",
+                  secondaryFiles = list(".bai"))
 req2 <- list(class = "DockerRequirement",
              dockerPull = "biocontainers/samtools:v1.7.0_cv3")
 req3 <- list(class = "InitialWorkDirRequirement",
